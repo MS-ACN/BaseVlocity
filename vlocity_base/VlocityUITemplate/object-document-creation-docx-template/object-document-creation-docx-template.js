26b0095d458c52dc2e47a904ec4cbd294f712da9
Adam Rutland <arutland@vlocity.com>
2018-09-02 09:26:40 -0700

    Update 900.169.0

--- vlocity_base/VlocityUITemplate/object-document-creation-docx-template/object-document-creation-docx-template.js
+++ vlocity_base/VlocityUITemplate/object-document-creation-docx-template/object-document-creation-docx-template.js
@@ -1,4 +1,4 @@
-baseCtrl.prototype.setIPScope = function(scp) {
+baseCtrl.prototype.setDocXIPScope = function(scp) {
     'use strict';
     var afterSlash = '/' + window.location.href.split('.com/')[1].split('/')[0];
     window.VlocOmniSI = scp;
@@ -10,7 +10,7 @@ baseCtrl.prototype.setIPScope = function(scp) {
     });
 
     window.addEventListener('message', function(event) {
-        if (event.data && event.data.constructor === Object && event.data.hasOwnProperty('docGenAttachmentId')) {
+        if (event.data && event.data.constructor === Object && event.data.hasOwnProperty('docGenContentVersionId')) {
             window.VlocOmniSI.applyCallResp(event.data);
         }
     }, false);
