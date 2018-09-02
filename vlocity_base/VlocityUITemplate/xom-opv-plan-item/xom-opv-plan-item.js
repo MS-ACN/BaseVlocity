724ff27da957a7906dd974336fd52e40739f9935
Adam Rutland <arutland@vlocity.com>
2018-09-02 09:16:50 -0700

    Update 900.131.0

--- vlocity_base/VlocityUITemplate/xom-opv-plan-item/xom-opv-plan-item.js
+++ vlocity_base/VlocityUITemplate/xom-opv-plan-item/xom-opv-plan-item.js
@@ -1,14 +1,21 @@
 vlocity.cardframework.registerModule
     .controller('actionRedirectController',
                  ["$scope","$rootScope","$window","$location", function( $scope,$rootScope,$window,$location){
-        this.performAction =  function (action, params,itemId) {
+        this.performAction =  function (action, params,itemId,customTaskExecUrl) {
           if (params) {
             var nsPrefix = $rootScope.nsPrefix;
-            var url = action[nsPrefix + 'Url__c'] || action[nsPrefix + 'URL__c'] || action.url;
-            action[nsPrefix + 'UrlParameters__c'] = itemId;
+            var url;
+            if(customTaskExecUrl) {
+                url = customTaskExecUrl;
+                console.log('customTaskExecUrl', customTaskExecUrl);
+                url=url.toString().replace("{0}",itemId);
+            } else {
+                url = action[nsPrefix + 'Url__c'] || action[nsPrefix + 'URL__c'] || action.url;
+                action[nsPrefix + 'UrlParameters__c'] = itemId;
 
-            url=url.toString().replace("{0}",itemId);
-            action[nsPrefix + 'URL__c']=url;
+                url=url.toString().replace("{0}",itemId);
+                action[nsPrefix + 'URL__c']=url;
+            }
             console.log('Card action', action);
             var isInConsole = (window.sforce && sforce.one)
             if(isInConsole){
